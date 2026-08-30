53657370334491dc3a5cdd85a321b2b0dc89786b
Neel Avaiya <neelavaiya201@gmail.com>
2026-08-30 14:58:41 +0530

    feat: integrate better-auth for authentication and add auth configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = process.env.PORT;
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/', (req, res) => {
     res.send("hello neel!")
